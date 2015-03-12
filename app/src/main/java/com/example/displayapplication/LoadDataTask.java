68d1fdfc4219fd686a5e077f1ec2ce2d805e21e2
Irina Belenki <irina.belenki@gmail.com>
2015-03-12 19:37:13 +0200

    import is cleaned.

--- app/src/main/java/com/example/displayapplication/LoadDataTask.java
+++ app/src/main/java/com/example/displayapplication/LoadDataTask.java
@@ -1,7 +1,5 @@
 package com.example.displayapplication;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
